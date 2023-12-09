f4c83a33ee7d6bdbce566884a180c9b5515a7fc3
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2023-12-09 21:49:37 +0530

    feat: add networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,26 @@ const config: HardhatUserConfig = {
       chainId: 421613,
       accounts: [deployer]
     },
+    'mumbai': {
+      url: 'https://rpc.ankr.com/polygon_mumbai',
+      chainId: 80001,
+      accounts: [deployer]
+    },
+    'scroll-sepolia': {
+      url: 'https://sepolia-rpc.scroll.io',
+      chainId: 534351,
+      accounts: [deployer]
+    },
+    celo: {
+      url: 'https://alfajores-forno.celo-testnet.org',
+      chainId: 44787,
+      accounts: [deployer]
+    },
+    base: {
+      url: 'https://goerli.base.org',
+      chainId: 84531,
+      accounts: [deployer]
+    }
   }
 };
 
