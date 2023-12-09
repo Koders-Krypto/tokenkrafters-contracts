0d4293a2d746d33caa20aebbc36833bd3d7b21bc
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2023-12-09 20:47:37 +0530

    feat: deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,17 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import 'dotenv/config'
 
+const deployer = process.env.PRIVATE_KEY || '';
+
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    'arb-goerli': {
+      url: 'https://arbitrum-goerli.publicnode.com',
+      chainId: 421613,
+      accounts: [deployer]
+    },
+  }
 };
 
 export default config;
